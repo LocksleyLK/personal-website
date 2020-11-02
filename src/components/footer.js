eb3fa7429208fa1f25a2efda9ca05c537ed58fdd
Locksley Kolakowski <locksleylk@gmail.com>
2020-11-01 20:47:02 -0800

    fix copyright padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 
 export default function Footer() {
   return (
-    <div className="mt-16 mb-6 mx-auto max-w-3xl flex justify-center text-gray-900 dark:text-gray-100 px-4">
+    <div className="bottom mt-16 mb-6 mx-auto max-w-3xl flex justify-center text-gray-900 dark:text-gray-100 px-4">
       <span className="text-sm">🦝 Copyright &copy; Locksley Kolakowski {new Date().getFullYear()} | All opinions are my own</span>
     </div>
   )
