22966292d804e90eb53de033d9c4470cb42826f6
Locksley Kolakowski <locksleylk@gmail.com>
2020-12-01 00:21:03 -0800

    fixed quantum sizing

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,12 @@ function Card(props) {
   return (
     <div class="rounded overflow-hidden shadow-lg dark:bg-gray-800 dark:text-gray-100">
       <Link to={props.link}>
-        <div className="aspect-w-3 aspect-h-2">
+        <div
+          className="aspect-w-3 aspect-h-2"
+          style={{
+            position: "relative",
+            paddingBottom: "calc(2 / 3 * 100%)",
+          }}>
           <img class="w-full" src={props.image} alt={props.alt} />
         </div>
         <div class="px-6 py-4">
